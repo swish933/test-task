36afe177deeb61836cb7b57ae8b49a2da36fdd94
Ikem Nomso <ikemnomso@gmail.com>
2024-03-28 16:48:44 +0100

    added .env sample

--- server.js
+++ server.js
@@ -2,11 +2,24 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const { connect } = require("./database/connection");
+const { generateMiddleWare } = require("./middleware/generateMiddleware");
+const { registerSchema } = require("./validation/register.validation");
+const cors = require("cors");
 
 dotenv.config();
+app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT;
-app.get("/", (req, res) => res.send("Hello World!"));
+app.post("/register", generateMiddleWare(registerSchema), (req, res) => {
+	res
+		.status(201)
+		.send({ message: "User registration successful", data: req.body });
+});
+
+app.all("*", (req, res) => {
+	res.status(404).send({ message: "Route not found!" });
+});
 
 connect().then(() => {
 	console.log("Connected to MongoDB");
