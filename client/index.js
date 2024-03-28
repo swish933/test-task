37f7cc06f3fbf5d66f80b4b066388f9bd5b5f9b0
Ikem Nomso <ikemnomso@gmail.com>
2024-03-28 21:27:56 +0100

    added form and register function

---
+++ client/index.js
@@ -0,0 +1,55 @@
+const form = document.querySelector("#userinfo");
+
+async function registerUser() {
+	const formData = new FormData(form);
+	let serialized = Object.fromEntries(formData);
+	let stringifiedData = JSON.stringify(serialized);
+	let theDiv;
+	let newContent;
+	let newElem;
+
+	try {
+		const response = await fetch("http://localhost:5000/register", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: stringifiedData,
+		});
+
+		const data = await response.json();
+
+		if (data.errors) {
+			console.log(data.errors);
+			const err = data.errors.details[0];
+			console.log(err);
+			theDiv = document.querySelector("#error-msg");
+			newElem = document.createElement("p");
+			newElem.setAttribute("id", "alert");
+			newContent = document.createTextNode(err.message);
+			newElem.appendChild(newContent);
+			theDiv.appendChild(newElem);
+			theDiv.classList.remove("hide");
+		} else {
+			theDiv = document.querySelector("#success-msg");
+			newElem = document.createElement("p");
+			newElem.setAttribute("id", "alert");
+			newContent = document.createTextNode(data.message);
+			newElem.appendChild(newContent);
+			theDiv.appendChild(newElem);
+			theDiv.classList.remove("hide");
+		}
+
+		setTimeout(() => {
+			const alert = document.querySelector("#alert");
+			theDiv.classList.add("hide");
+			alert.remove();
+		}, 3500);
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+// Take over form submission
+form.addEventListener("submit", (ev) => {
+	ev.preventDefault();
+	registerUser();
+});
